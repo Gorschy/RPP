d7f7e8a9d898a73b254a61b322f419a89ce7a68a
jetaxe100 <57256192+jetaxe100@users.noreply.github.com>
2021-10-11 03:28:38 +1100

    Connection between BE and FE; interactivity etc.

--- src/pages/solutions.js
+++ src/pages/solutions.js
@@ -1,10 +1,11 @@
-import React, { useMemo, useState, useContext } from 'react';
+import React, { useMemo, useState, useContext, Component } from 'react';
 import { List, Row, Col, Modal, Button, Divider, Progress, Tooltip, InputNumber } from 'antd';
 import { Link } from 'react-router-dom';
 import { UserContext } from './UserContext';
-import { Auth } from 'aws-amplify';
+import { Auth, API, graphqlOperation } from 'aws-amplify';
+import {listSolutions} from '../graphql/queries';
 import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, LayersControl, LayerGroup} from 'react-leaflet';
 import '../style.css';
 import './solutions.css';
 import 'leaflet/dist/leaflet.css';
@@ -14,161 +15,43 @@ import treeIcon from '../assets/treeIcon50x50.png';
 COMMENTS BY: Zachary O'Reilly-Fullerton
 ======================================== */
 
-
-
-// Variable initialization.
-// This, as you may imagine is the beginning level of zoom for the map. The higher the number, the higher the zoom. Personally,
-// I think that a standard zoom of 2 is excellent, however as the map element size shrinks, we may increase the zoom level.
 const startingZoom = 2; 
-// The starting location of the map, whilst this is so zoomed out, it has no real point, however we may change this later.
+var waypointList = [];
+var usersName;
 const startVariables = [
     0, //Start Latitude
     0, //Start Longitude
 ];
-
-
-
-
-// This is the details for the custom icon that we have used, the image of the tree... which was screenshotted from an email
-// with Aymeric himself, how fancy. The anchor values are where the popup ties to. Please for the love of all that is nice
-// in the world DO NOT CHANGE THIS UNLESS YOU KNOW WHAT YOU ARE DOING!
 const waypointIcon = L.icon({ 
     iconUrl: treeIcon,
     iconSize: [50,50],
     iconAnchor: [25.5, 50],
     popupAnchor: [0, -50]
 })
-
-
-// This is the standard list of waypoints to add to the map. This array is added to by the call to the database.
-// For the moment it is filled with placeholder data for proof of concept.
-var waypointList = []
-    // waypointList objects follow the nomenclature of [name, description, position (consisting of lat and longitude)].
-    var populateArray = [
-
-    /*
-    Zac’s Summary
-
-    Title: "Bat And Agave Conservation Project"
-    Description: "Assist and enhance agave and bat populations."
-    Coordinates: [32.248000, -112.916100]
-
-    Title: "Blue Heart Mangrove Restoration"
-    Description: "Preservation and restoration of red mangrove sites in the ‘Blue Heart’ zone and potentially coastal area within the Sunshine Coast Council Group constituency."
-    Coordinates: [-26.5175, 153.0341]
-
-    Title: "Borneo Orangutan Project"
-    Description: "Assist and protect orangutans and their forest homes."
-    Coordinates: [0.1378, 117.4970]
-
-    Title: "CCC Athens Recovery"
-    Description: "Reestablishment of landscape after wildfire."
-    Coordinates: [37.9464, 23.8167]
-
-    Title: "Daintree Rainforest Conservation"
-    Description: "Restore previously farmed land to its original rainforest condition. The Daintree Rainforest is the oldest rainforest on Earth around 180 million years old – older than the Amazon Rainforest and one of the best biologically diverse rainforests in the world."
-    Coordinates: [-16.26188066268747, 145.4441841548669]
-
-*/
-    {
-        "Name"        : "Bat And Agave Conservation Project",
-        "Description" : "Assist and enhance agave and bat populations.",
-        "Position"    : [32.248000, -112.916100],
-        "MaxPurchases": 100,
-        "CurrentPurchases"  : 56
-    }, 
-    {
-        "Name"        : "Blue Heart Mangrove Restoration",
-        "Description" : "Preservation and restoration of red mangrove sites in the ‘Blue Heart’ zone and potentially coastal area within the Sunshine Coast Council Group constituency.",
-        "Position"    : [-26.5175, 153.0341],
-        "MaxPurchases": 100,
-        "CurrentPurchases"  : 86
-    },
-    {
-        "Name"        : "Borneo Orangutan Project",
-        "Description" : "Assist and protect orangutans and their forest homes.",
-        "Position"    : [0.1378, 117.4970],
-        "MaxPurchases": 100,
-        "CurrentPurchases"  : 15
-    },
-    {
-        "Name"        : "CCC Athens Recovery",
-        "Description" : "Reestablishment of landscape after wildfire.",
-        "Position"    : [37.9464, 23.8167],
-        "MaxPurchases": 100,
-        "CurrentPurchases"  : 12
-    },
-    {
-        "Name"        : "Daintree Rainforest Conservation",
-        "Description" : "Restore previously farmed land to its original rainforest condition. The Daintree Rainforest is the oldest rainforest on Earth around 180 million years old – older than the Amazon Rainforest and one of the best biologically diverse rainforests in the world.",
-        "Position"    : [-16.26188066268747, 145.4441841548669],
-        "MaxPurchases": 100,
-        "CurrentPurchases"  : 5
-    }
-];
-
-// This is the function that takes an inputted array, theoretically from a database and incrementally pushes it onto the
-// above array of waypoints. Pretty simple stuff.
-function populateMarkers(solutionsArray) {
-    // solutionsArray theoretically = [name, description, position]
-    solutionsArray.forEach(solutionItem => {
-        waypointList.push(
-        {
-            "Name"        : solutionItem.Name,
-            "Description" : solutionItem.Description, 
-            "Position"    : solutionItem.Position,
-            "MaxPurchases": solutionItem.MaxPurchases,
-            "CurrentPurchases"  : solutionItem.CurrentPurchases
-        }
-    ) 
-    });
-};
-
-
-// Here is where the butter meets the bread and we get moving.
-// A map is memoized and is displayed with the variable element of "Waypoints".
-// The variable element is created below.
-function CreateMap() {
-
-    // Creates the content to be returned.
-    const DisplayMap = useMemo( () => (
-        <MapContainer 
-            id="map" 
-            center={startVariables} 
-            zoom={startingZoom} 
-            scrollWheelZoom={true}
-            maxBounds={[[-90, -180],[90, 180]]}
-        >
-        <TileLayer  
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            minZoom='2'
-            noWrap="true"
-        />
-        <Waypoints />
-        </MapContainer>
-    ), [],)
-    
-    return (
-        <div>
-        {DisplayMap}
-        </div>
-    )
+var itemToPass = {
+    "title"             : "",
+    "desc"              : "",
+    "Position"          : [0, 0],
+    "MaxPurchases"      : 100,
+    "CurrentPurchases"  : 0
 }
+const calcPercent = (CurrentPurchases, MaxPurchases) => { return Math.floor((CurrentPurchases / MaxPurchases)* 100); }
+const calcRemainingSpots = (val1, val2) => { return (val1-val2); }
+const updateCost = (inputVal) => {document.getElementById("costDiv").innerHTML = 20* inputVal + ".00";}
+var firstName;
+var carbonScore = 20;
+var carbonOffset = 140;
 
-// This function takes the waypoint list and maps it to the marker tags. It may not look like much
-// but this is the resultant code of 8 hours of research and struggle.
 function Waypoints () {
+    console.log(waypointList);
     return (
-        waypointList.map((waypoint) => (
-            <React.Fragment key={waypoint.Name}>
-            <Marker position={(waypoint.Position)} icon={waypointIcon}>
+        waypointList.map((LOI) => (
+            <React.Fragment key={LOI.title}>
+            <Marker position={LOI.coords} icon={waypointIcon}>
                 <Popup>
-                    <div>   
-                    <div className="markerHeader"><b>{waypoint.Name}</b></div> 
-                        <br/>
-                    <div className="markerDescription">{waypoint.Description}</div>
-                    </div>
+                    <div className="markerHeader"><b>{LOI.title}</b></div> 
+                    <br/>
+                    <div className="markerDescription">{LOI.desc}</div>
                 </Popup>
             </Marker>
             </React.Fragment>
@@ -176,43 +59,14 @@ function Waypoints () {
     );
 }
 
-// Function call to the populateMarkers function with a bit of test code for proof of concept.
-
-populateMarkers(populateArray);
-var itemToPass = {
-    "Name"              : "",
-    "Description"       : "",
-    "Position"          : [0, 0],
-    "MaxPurchases"      : 100,
-    "CurrentPurchases"  : 0
-}
-
-const calcPercent = (CurrentPurchases, MaxPurchases) => { return Math.floor((CurrentPurchases / MaxPurchases)* 100); }
-const calcRemainingSpots = (val1, val2) => { return (val1-val2); }
-const updateCost = (inputVal) => {document.getElementById("costDiv").innerHTML = 20* inputVal + ".00";}
-var firstName;
-var carbonScore = 20;
-var carbonOffset = 140;
 function CreateList () {
-    const getUser = async () => {
-    
-        var user = await Auth.currentAuthenticatedUser();
-        const { attributes } = user;
-        firstName = (attributes["given_name"]);
-    }
-    getUser();
-
+    const {loggedIn, setLoggedIn} = useContext(UserContext);
     const [isModalVisible, setIsModalVisible] = useState(false);
     const showModal = (passedItem) => {
         itemToPass = passedItem;
         setIsModalVisible(true);
     };
     const handleCancel = () => {setIsModalVisible(false);}
-
-    //Capture current authed user
-    const {loggedIn} = useContext(UserContext);
-
-    
 
     return(
         <div>
@@ -224,15 +78,14 @@ function CreateList () {
         itemLayout={'vertical'}
         header={<div><h1>Solutions</h1><br/><h3>Click a solution to invest.</h3></div>}
         renderItem={item => (
-            <List.Item onClick={() => showModal(item)}>
+            <List.Item className="solListItem" onClick={() => showModal(item)}>
                 <List.Item.Meta
-                    key = {item.Name}
-                    title={<h2>{item.Name}</h2>}
-                    description={<div className="standardText">{item.Description}</div>}
+                    title={<h2>{item.title}</h2>}
+                    description={<div className="standardText">{item.desc}</div>}
                 />     
-                <Tooltip autoAdjustOverflow = {true} className="progressTT" title={<h3>{item.CurrentPurchases} of {item.MaxPurchases} units purchased.</h3>}
+                <Tooltip autoAdjustOverflow = {true} className="progressTT" title={<h3>{item.filledP} of {item.totalP} units purchased.</h3>}
                     color={"#ffffff"}>
-                    <Progress   className="progressBar" showInfo={false} percent={calcPercent(item.CurrentPurchases, item.MaxPurchases)} 
+                    <Progress   className="progressBar" showInfo={false} percent={calcPercent(item.filledP, item.totalP)} 
                                 strokeColor={{"0%" : "#4A7634","100%" : "#134A2C",}} />
                 </Tooltip>
                 
@@ -241,7 +94,7 @@ function CreateList () {
         ></List>
         
 
-        <Modal className="purchaseModal" title={<h1>Invest in: {itemToPass.Name}</h1>} centred visible={isModalVisible} width="90vw" closable={false}
+        <Modal className="purchaseModal" title={<h1>Invest in: {itemToPass.title}</h1>} centred visible={isModalVisible} width="90vw" closable={false}
             footer={[<Button className="cancelButton" onClick={handleCancel}>Back</Button>]} 
         >
 
@@ -249,14 +102,14 @@ function CreateList () {
                 <div>
                     <Row justify="space-around">
                     <Col span={12}>
-                    <h3><b>{itemToPass.Description}</b></h3>
+                    <h3><b>{itemToPass.desc}</b></h3>
                     <br/>
                     <Row>
                         <Col span={6}><Progress type="circle" strokeColor={{"0%" : "#4A7634","100%" : "#134A2C",}} 
-                            percent={calcPercent(itemToPass.CurrentPurchases, itemToPass.MaxPurchases)}/></Col>
+                            percent={calcPercent(itemToPass.filledP, itemToPass.totalP)}/></Col>
                         <Col span={18}><h2>
-                            {itemToPass.CurrentPurchases} spaces have been filled out of the needed {itemToPass.MaxPurchases}.<br/>
-                            {calcRemainingSpots(itemToPass.MaxPurchases, itemToPass.CurrentPurchases)} spaces remain. <br/>
+                            {itemToPass.filledP} spaces have been filled out of the needed {itemToPass.totalP}.<br/>
+                            {calcRemainingSpots(itemToPass.totalP, itemToPass.filledP)} spaces remain. <br/>
                             Each space represents 1 carbon tonne. <br/>
                             1 carbon tonne = $20 AUD.
                         </h2></Col>
@@ -264,14 +117,10 @@ function CreateList () {
                     <br/>
                     <Row><Col span={12}>
                         <h3>How many units would you like to purchase? </h3> </Col><Col span={12}>
-                        <InputNumber id="purchaseNumberInput" min={1} max={calcRemainingSpots(itemToPass.MaxPurchases, itemToPass.CurrentPurchases)} 
+                        <InputNumber id="purchaseNumberInput" min={1} max={calcRemainingSpots(itemToPass.totalP, itemToPass.filledP)} 
                             defaultValue = {1} onChange={updateCost} step={1}/>
                     </Col></Row>
                     <h3>Total Cost: $<span id="costDiv">20.00</span></h3>
-
-
-
-
 
                     </Col>
                     <Divider type="vertical" id="solutionModalVertDivider"/>
@@ -280,7 +129,7 @@ function CreateList () {
                     
                     <Row>
                     <Col span={18}>
-                        <h2>Hello, <b>{firstName}</b>.<br/>
+                        <h2>Hello, <b>{usersName}</b>.<br/>
                         You have <b>{carbonScore}</b> tonnes of excess carbon 
                         emission that hasn't been offset. <br/>
                         You have offset a total of <b>{carbonOffset}</b> tonnes
@@ -290,7 +139,7 @@ function CreateList () {
                         <Tooltip autoAdjustOverflow = {true} className="progressTT" title={<h3>{carbonOffset}/{carbonOffset+carbonScore} Carbon Tonnes offset.</h3>}
                             color={"#ffffff"}>
                         <Progress className="progressBar" percent={calcPercent(carbonOffset, (carbonOffset+carbonScore))} 
-                            strokeColor={{"0%" : "#4A7634","100%" : "#134A2C",}} trailColor = {"#8b0000"} type="circle"/>
+                            strokeColor={{"0%" : "#4A7634","100%" : "#134A2C",}} type="circle"/>
                         </Tooltip>
                     </Col>
                     </Row>
@@ -313,17 +162,89 @@ function CreateList () {
 
 }
 
+function populateMarkers(solutionsArray) {
+    solutionsArray.forEach(solutionItem => {
+        waypointList.push(
+        {
+            "title"         : solutionItem.title,
+            "desc"          : solutionItem.desc,
+            "coords"        : [parseFloat(solutionItem.coordX), parseFloat(solutionItem.coordY)],
+            "filledP"       : parseFloat(solutionItem.filledP),
+            "totalP"        : parseFloat(solutionItem.totalP),
+            "type"          : solutionItem.type,
+            //"funding"       :
+            "backerCount"   : solutionItem.backerCount
+        }
+    ) 
+    });
+};
+
 // Finally, the actual section that renders the page.
-const Solutions = () => { 
-    return(
-        <div>
-            <Row>
-            <CreateMap  />
-            
-            <CreateList />
-            </Row>
-        </div>
-    ); 
+class Solutions extends Component { 
+    constructor(props) {
+        super(props)
+        this.deleteInput = null;
+    }
+
+    state = {
+        user: null,
+        loaded: false
+    }
+
+    async componentDidMount () {
+        var currentUser = await Auth.currentAuthenticatedUser();
+        const { attributes } = currentUser;
+        this.setState({user: currentUser})
+        usersName = this.state.user.attributes.given_name
+
+        const getSols = await API.graphql(graphqlOperation(listSolutions));
+        const returnArray = getSols.data.listSolutions.items;
+
+        populateMarkers(returnArray)
+        this.setState({loaded: true})
+    }
+
+    
+    render() {
+        return(
+            <div>
+                <Row>
+                    <MapContainer 
+                    id="map" 
+                    center={startVariables} 
+                    zoom={startingZoom} 
+                    scrollWheelZoom={true}
+                    maxBounds={[[-90, -180],[90, 180]]}
+                    >
+                    <TileLayer  
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        minZoom='2'
+                        noWrap="true"
+                    />
+                    <LayersControl position="topright">
+                        <LayersControl.Overlay checked name="Solution Type 1">
+                            <LayerGroup>
+                            <Waypoints />
+                            </LayerGroup>
+                        </LayersControl.Overlay>
+                        <LayersControl.Overlay checked name="Solution Type 2">
+                        </LayersControl.Overlay>
+                        <LayersControl.Overlay checked name="Solution Type 3">
+                        </LayersControl.Overlay>
+                        <LayersControl.Overlay checked name="Solution Type 4">
+                        </LayersControl.Overlay>
+                    </LayersControl>
+                    
+                    </MapContainer>
+                
+                <CreateList />
+                </Row>
+            </div>
+        ); 
+
+    }
+    
 }
 
 export default Solutions;
